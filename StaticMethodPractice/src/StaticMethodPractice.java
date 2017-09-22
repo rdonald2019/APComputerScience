58f6dcf78eb3cd7bce4eccb5b26d85e778742189
rdonald2019 <rdonald2019@cchsraider.net>
2017-09-22 10:08:30 -0400

    initial commit

---
+++ StaticMethodPractice/src/StaticMethodPractice.java
@@ -0,0 +1,27 @@
+
+public class StaticMethodPractice {
+
+	public static void main(String[] args) {
+		myBox(); //Prints Top Box
+		myBox(); //Prints Bottom Box
+	}
+
+	public static void myBox() {
+		//Prints out 1 box
+		solidLine();
+		brokenLine();
+		brokenLine();
+		brokenLine();
+		solidLine();
+	}
+	
+	public static void solidLine() {
+		//Prints top and bottom line of each box
+		System.out.println("***********");
+	}
+	
+	public static void brokenLine() {
+		//Prints the sides of the boxes
+		System.out.println("*         *");
+	}
+}
